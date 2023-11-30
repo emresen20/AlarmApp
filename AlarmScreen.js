110c76780304fd48310da17f95c82e876b8a7dad
emresen20 <esen20@posta.pau.edu.tr>
2023-11-30 21:50:40 +0300

    Created app and bottom navigator. l

---
+++ AlarmScreen.js
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native"
+
+
+
+export const AlarmScreen= ({navigation})=>{
+    return(
+        <View>
+            <Text>
+                AlramScreeen
+                </Text>
+        </View>
+    )
+}
+
---
+++ App.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { SettingsScreen } from './SettingsScreen';
+import { AlarmScreen } from './AlarmScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons'; // İkonların yüklenmesi için 'Ionicons' paketini düzelttim.
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName='AlarmScreen'
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === 'AlarmScreen') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'SettingsScreen') { // Burada 'SettingsScreen' olarak kontrol edilmeli
+              iconName = focused ? 'settings' : 'settings-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'blue',
+          tabBarInactiveTintColor: 'green',
+        })}
+      >
+        <Tab.Screen name="AlarmScreen" component={AlarmScreen} options={{ headerShown: false }}/>
+        <Tab.Screen name="SettingsScreen" component={SettingsScreen}  options={{ headerShown: false }} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
